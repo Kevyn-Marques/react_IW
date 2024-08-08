e51221de9050ffda3978843fec63da6c9d6606a4
Kevyn-Marques <takinhoverycrazy@gmail.com>
2024-08-08 10:59:42 -0300

    inclusão do component Banner e Container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 function App() {
   return (
     <>
     <Header/>
-    <h1>Etec-MCM</h1>
-    <p>Hello World</p>
+    <Banner/>
+    <Container>
+      <h1>Etec-MCM</h1>
+      <p>Hello World</p>
+    </Container>
+    <Footer/>
     </>
 );
 }
