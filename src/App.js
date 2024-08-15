be3b2f41a2a8373bc9cd7eec626e225039bad7a8
Kevyn-Marques <takinhoverycrazy@gmail.com>
2024-08-15 12:34:16 -0300

    avanços

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
     <Header/>
-    <Banner/>
+    <Banner image = "favoritos" />
     <Container>
-      <h1>Etec-MCM</h1>
-      <p>Hello World</p>
+      <h1>Filmes</h1>
+      <section className="cards">
+        <Card id="B_nq7VTJZds"/>
+        <Card id="B_nq7VTJZds"/>
+        <Card id="B_nq7VTJZds"/>
+        <Card id="B_nq7VTJZds"/>
+        <Card id="B_nq7VTJZds"/>
+      </section>
     </Container>
     <Footer/>
     </>
